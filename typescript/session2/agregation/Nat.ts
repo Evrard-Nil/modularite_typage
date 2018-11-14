58073bf20e9e90528aba5c8750bf385d4f906e8d
Lucille <lucille.gillard@macroprog.com>
2018-11-14 18:54:47 +0100

    add: Nat

---
+++ typescript/session2/agregation/Nat.ts
@@ -0,0 +1,9 @@
+import {SemiAnneauUnitaireEuclidien} from "../../structuresAlgebriques/hierarchie";
+import {FabriqueNaturels} from "../../session1/td/Nat/FabriqueNaturels";
+import {EtatNaturel} from "./EtatNaturel";
+import {EtatNaturelPur} from "./EtatNaturelIPur";
+
+export interface Nat extends SemiAnneauUnitaireEuclidien<Nat>, FabriqueNaturels<Nat>, EtatNaturel<Nat>{
+    creerNatAvecEtat(etat : EtatNaturelPur) : Nat;
+    etat() : EtatNaturelPur;
+}
