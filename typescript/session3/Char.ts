5892d520cbc5567a0a8a6cbb47694dc4c7a854dc
evrard-nil <evrard-nil.daillet@soprasteria.com>
2018-11-22 09:49:07 +0100

    Session3 - TS : Implémentation de l'itnerface Mot et Char

---
+++ typescript/session3/Char.ts
@@ -0,0 +1,11 @@
+export class Char {
+
+  private letter: string;
+
+  constructor(s: string) {
+    if (s.length <= 1) {
+      this.letter = s;
+    }
+    else throw new Error('Un charatère ne peut être de longeur supérieur à 1');
+  }
+}
